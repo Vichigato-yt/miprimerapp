00606fc1e12bf08cc5b6f9e624fbd5a83feab131
vicejo1009 <vicejo1009@gmail.com>
2025-09-03 21:14:54 +0000

    Hola mundo desde el WelcomeText

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { WelcomeText } from "@/components/WelcomeText";
 
 export default function Index() {
   return (
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hola mundo!!</Text>
+      <WelcomeText> </WelcomeText>
     </View>
   );
 }
---
+++ components/WelcomeText.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+export function WelcomeText():any{
+    return <>
+    <Text>Hola Mundo</Text>;
+    <Text>PEPE</Text>;
+    </>
+}
